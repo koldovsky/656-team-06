4502cd119fef98cffee00d30671554b77c3d3ea7
MargaritaChorniak <chorniak.rita@gmail.com>
2021-12-29 17:08:50 +0200

    add burger

---
+++ js/burger.js
@@ -0,0 +1,9 @@
+(function () {
+    let menuBtn = document.querySelector('.menu-btn');
+    let menu = document.querySelector('.header__menu');
+
+    menuBtn.addEventListener('click', function () {
+        menuBtn.classList.toggle('active');
+        menu.classList.toggle('active');
+    });
+})(); 
