475842070d8a3eb4f7eddd48085cd3dea9bdc772
Serhii-Chernikov <peregrin.took1939@gmail.com>
2021-12-31 11:51:35 +0000

    js

--- js/carousel.js
+++ js/carousel.js
@@ -1,6 +1,6 @@
 (function () {
     const slides = [
-       "<div class="w-block-1">
+       `<div class="w-block-1">
         <img class="w-image" src="img/cristmas-family-1.webp" alt="happy family with a dog"/>
         <p class="names">Dean and Alice Peterson</p>
         <p class="w-text-content">“Christmas is a special time! We always buy Christmas decorations in advance.
@@ -8,16 +8,16 @@
             purchased."
         </p>
         <p>December 19, 2021</p>
-    </div>",
-        "<div class="w-block-2">
+    </div>`,
+        `<div class="w-block-2">
     <img class="w-image" src="img/cristmas-family-2.webp"  alt="mother and daughter"/>
     <p class="names">Susan Owler</p>
     <p class="w-text-content">“My daughter is 4 years old and she has been waiting for Christmas
         preparations since October! We start decorating the Christmas tree in early December, and I always buy something new at Christmas
         Decorations Shop.”</p>
     <p>December 21, 2021</p>
-</div>",
-        "<div class="w-block-3">
+</div>`,
+        `<div class="w-block-3">
 <img class="w-image"src="img/cristmas-family-3.webp"  alt="mother and daughter with the tree"/>
 <p class="names">Daniella Lexington</p>
 <p class="w-text-content"><span class="w-text-content" style="display: block;">“The anticipation of Christmas is the best thing in December. I like the
@@ -25,33 +25,36 @@
     of baubles.”</span>
 </p>
 <p>Decmber 23, 2021</p>
-</div>"
+</div>`
     ];
 
     let currentSlide = 0;
+    let secondSlide = 1;
 
     function showCurrentSlide() {
         const slideContainer = document.querySelector('.track-clients');
         slideContainer.innerHTML = slides[currentSlide];
-        if (window.innerWidth > 700) {
-            const secondSlide = currentSlide + 1 >= slides.length ? 0 : currentSlide + 1;
-            slideContainer.innerHTML += slides[secondSlide];
-            if (window.innerWidth > 1000) {
-                const thirdSlide = secondSlide + 1 >= slides.length ? 0 : secondSlide + 1;
-                slideContainer.innerHTML += slides[thirdSlide];
-            }
-
-        }
+        secondSlide = currentSlide + 1 >= slides.length ? 0 : currentSlide + 1;
+        slideContainer.innerHTML += slides[secondSlide];     
     }
-    function nextSlide() {
-        currentSlide = currentSlide + 1 >= slides.length ? 0 : currentSlide + 1;
+
+    function nextSlide() { 
+        if(secondSlide + 1 >= slides.length) {
+            return;
+        }
+        currentSlide = currentSlide + 1;
         showCurrentSlide();
     }
 
     function prevSlide() {
-        currentSlide = currentSlide - 1 <= 0 ? slides.length - 1 : currentSlide - 1;
+        if(secondSlide - 1 <= 0) {
+        return;
+        }
+        currentSlide = currentSlide - 1;
         showCurrentSlide();
     }
+
+    showCurrentSlide();
 
     document.querySelector('.next.button').addEventListener('click', nextSlide);
     document.querySelector('.prev.button').addEventListener('click', prevSlide);
