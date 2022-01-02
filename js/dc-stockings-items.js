54f0a74413c352766950a788f58c1b9f763127d8
RomanStefanyshyn <motivnewex@gmail.com>
2022-01-02 18:28:37 +0000

    script

--- js/dc-stockings-items.js
+++ js/dc-stockings-items.js
@@ -1,13 +1,13 @@
 decorations-category-stocking
 (async function () {
-    const response = await fetch('decorations-category-stocking-items.json');
+    const response = await fetch('decorations-category-stockings-items.json');
     const products = await response.json();
   
     function renderProducts(products) {
-      const productsContainer = document.querySelector('.stocking__items');
+      const productsContainer = document.querySelector('.stockings__items');
       for (const product of products) {
         productsContainer.innerHTML +=
-          `<div class="decorations-category-stocking__item">
+          `<div class="decorations-category-stockings__item">
           <img src="${product.image}" alt="${product.id}">
           <span>${product.title}</span>
           <strong>$${product.price}</strong>
