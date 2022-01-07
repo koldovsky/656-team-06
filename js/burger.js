ceede180407410ff70c7a858b66c76af086951ac
MargaritaChorniak <chorniak.rita@gmail.com>
2022-01-07 15:51:56 +0200

    add block

--- js/burger.js
+++ js/burger.js
@@ -1,9 +1,9 @@
 (function () {
   const iconBurger = document.querySelector(".navigation-burger");
-  const navigationsBody = document.querySelector(".header__container");
+  const navigationsBody = document.querySelector(".header__menu");
   iconBurger.addEventListener("click", function (e) {
     document.body.classList.toggle("lock");
-    iconBurger.classList.toggle("active");
-    navigationsBody.classList.toggle("active");
+    iconBurger.classList.toggle("_active");
+    navigationsBody.classList.toggle("_active");
   });
 
