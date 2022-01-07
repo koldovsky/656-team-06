b44b8f839dcee613fbd5b5a7bed7dd41805837d3
MargaritaChorniak <chorniak.rita@gmail.com>
2022-01-07 16:52:34 +0200

    add burger2

--- js/burger.js
+++ js/burger.js
@@ -1,13 +1,10 @@
 (function(){
   const iconBurger = document.querySelector(".navigation-burger");
   const navigationsBody = document.querySelector(".header__menu");
-  console.log(iconBurger)
   iconBurger.addEventListener("click", function (e) {
-    console.log(111)
     document.body.classList.toggle("lock");
     iconBurger.classList.toggle("_active");
     navigationsBody.classList.toggle("header__menu_active");
   });
-
 })();
   
