7db4f55e2f474def7053a19e04c3de68485ff784
MargaritaChorniak <chorniak.rita@gmail.com>
2022-01-07 13:24:28 +0200

    add burger

--- js/burger.js
+++ js/burger.js
@@ -7,15 +7,3 @@
     navigationsBody.classList.toggle("active");
   });
 
-  const navLinks = document.querySelectorAll(".navigation__link");
-  navLinks.forEach((navLink) => {
-    navLink.addEventListener("click", (_) => {
-          if (iconBurger.classList.contains("active")) {
-              document.body.classList.remove("lock");
-              iconBurger.classList.remove("active");
-              navigationsBody.classList.remove("active");
-          }
-        }
-    );
-  });
-})();
