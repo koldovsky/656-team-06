e4d6b195692141af6881e61104a151473b3f8e84
AnnAYashkova <95094784+AnnAYashkova@users.noreply.github.com>
2024-07-23 20:48:13 +0300

    Delete js directory

--- js/clock.js
+++
@@ -1,8 +0,0 @@
-(function () {
-    function updateClock() {
-        const clockContainer = document.querySelector('.clock');
-        clockContainer.innerText = new Date().toLocaleTimeString();
-    }
-    updateClock();
-    setInterval(updateClock, 1000);
-})(); 
